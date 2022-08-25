75f1fbaf19051da69be87e34a31ca0b0271aa665
Victor Sol <106210473+victor-sol@users.noreply.github.com>
2022-08-25 19:07:23 -0300

    Adicionei DropDown, Button e Form
    Envio do segundo módulo do curso de React - Alura.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <Banner />
-      <Formulario/>
+      <Formulario />
     </div>
   );
 }
--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,17 +1,36 @@
+import Botao from '../Botão'
 import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
 
 
 const Formulario = () => {
+
+    const função = [
+        'Hard Carry',
+        'Mid',
+        'Offlane',
+        'Soft Support',
+        'Hard Support'
+
+    ]
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        console.log('Form foi submetido')
+    }
+
     return (
-    <section className="formulario">
-        <form>
-            <h2>Preencha os dados para criar o card do Doteiro do YOLO</h2>
-            <CampoTexto label="GamerTAG" placeholder="Digite sua GamerTAG..." />
-            <CampoTexto label="Posição In-Game" placeholder="Digite sua Posição In-Game..." />
-            <CampoTexto label="Imagem do Herói Favorito" placeholder="Informe o endereço da imagem..." />
-        </form>
-    </section>
+        <section className="formulario">
+            <form onSubmit={aoSalvar}>
+                <h2>Preencha os dados para criar o card do Doteiro do YOLO</h2>
+                <CampoTexto obrigatorio={true} label="GamerTAG" placeholder="Digite sua GamerTAG..." />
+                <CampoTexto obrigatorio={true} label="Hero Pool" placeholder="Digite sua Hero Pool..." />
+                <CampoTexto label="Imagem do Herói Favorito" placeholder="Informe o endereço da imagem..." />
+                <ListaSuspensa obrigatorio={true} label="Função" itens={função} />
+                <Botao texto='Criar card' />
+            </form>
+        </section>
     )
 }
 
