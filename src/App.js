e38264083f1da650c60097b7d1d691a847c848c1
Victor Sol <106210473+victor-sol@users.noreply.github.com>
2022-08-29 15:38:49 -0300

    Guardando os valores do formulário
    Utilizando state para guardar os valores aplicados no formulário.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
 
 
 function App() {
+
+  const [jogadores, setJogador] = useState([])
+
+  const aoJogadorCadastrado = (jogador) => {
+    console.log(jogador)
+    setJogador([...jogadores, jogador])
+  }
+
   return (
     <div className="App">
       <Banner />
-      <Formulario />
+      <Formulario aoJogadorCadastrado={jogadores => aoJogadorCadastrado(jogadores)} />
     </div>
   );
 }
--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -2,9 +2,10 @@ import Botao from '../Botão'
 import CampoTexto from '../CampoTexto'
 import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
+import { useState } from 'react'
 
 
-const Formulario = () => {
+const Formulario = (props) => {
 
     const função = [
         'Hard Carry',
@@ -12,22 +13,56 @@ const Formulario = () => {
         'Offlane',
         'Soft Support',
         'Hard Support'
-
     ]
+
+
+    const [gamerTag, setGamerTag] = useState('')
+    const [heroPool, setHeroPool] = useState('')
+    const [imagem, setImagem] = useState('')
+    const [funcao, setFuncao] = useState('')
+
 
     const aoSalvar = (evento) => {
         evento.preventDefault()
-        console.log('Form foi submetido')
+        props.aoJogadorCadastrado({
+            gamerTag,
+            heroPool,
+            imagem,
+            funcao
+        })
     }
 
     return (
         <section className="formulario">
             <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do Doteiro do YOLO</h2>
-                <CampoTexto obrigatorio={true} label="GamerTAG" placeholder="Digite sua GamerTAG..." />
-                <CampoTexto obrigatorio={true} label="Hero Pool" placeholder="Digite sua Hero Pool..." />
-                <CampoTexto label="Imagem do Herói Favorito" placeholder="Informe o endereço da imagem..." />
-                <ListaSuspensa obrigatorio={true} label="Função" itens={função} />
+                <CampoTexto
+                    obrigatorio={true}
+                    label="GamerTAG"
+                    placeholder="Digite sua GamerTAG..."
+                    valor={gamerTag}
+                    aoAlterado={valor => setGamerTag(valor)}
+                />
+                <CampoTexto
+                    obrigatorio={true}
+                    label="Hero Pool"
+                    placeholder="Digite sua Hero Pool..."
+                    valor={heroPool}
+                    aoAlterado={valor => setHeroPool(valor)}
+                />
+                <CampoTexto
+                    label="Imagem do Herói Favorito"
+                    placeholder="Informe o endereço da imagem..."
+                    valor={imagem}
+                    aoAlterado={valor => setImagem(valor)}
+                />
+                <ListaSuspensa
+                    obrigatorio={true}
+                    label="Função"
+                    itens={função}
+                    valor={funcao}
+                    aoAlterado={valor => setFuncao(valor)}
+                />
                 <Botao texto='Criar card' />
             </form>
         </section>
